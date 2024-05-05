2d82fac1fe5e2d1c4729cd55baac179e95c950f7
Kuba <62606910+kubixDev@users.noreply.github.com>
2024-05-05 16:56:42 +0200

    initial commit

---
+++ src/main/java/com/kubixdev/bingr/BingrApplication.java
@@ -0,0 +1,11 @@
+package com.kubixdev.bingr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BingrApplication.class, args);
+	}
+}
